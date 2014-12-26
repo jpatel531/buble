8dd85a7d9c93dac7c525699b61ea94eb06e3f2af
Jamie Patel <jamie@notespublication.com>
2014-12-26 21:37:46 +0000

    Michael is able to read the request body

--- bublé.rb
+++ bublé.rb
@@ -1,6 +1,6 @@
 require 'socket'
 require 'uri'
-
+require 'json'
 
 WEB_ROOT = './public'
 
@@ -20,34 +20,37 @@ end
 
 def requested_file(request_line)
 	request_uri		= request_line.split(" ")[1]
-	path 			= URI.unescape(URI(request_uri).path)
-
+	path 					= URI.unescape(URI(request_uri).path)
 
 	clean = []
-
 	parts = path.split("/")
 
 	parts.each do |part|
-
 		next if part.empty? || part == "."
-
 		part == ".." ? clean.pop : clean << part
-
 	end
 
 	File.join(WEB_ROOT, *clean)
-
 
 end
 
 server = TCPServer.new 'localhost', 2345
 
-loop do 
+loop do
 
-	socket = server.accept
-	request_line = socket.gets
+	socket 				= server.accept
+	request_line 	= socket.gets
 
 	STDERR.puts request_line
+
+	request_method = request_line.scan(/\w+/).first
+
+	puts request_method
+
+	data = socket.readpartial(1024).split("\r\n\r\n")
+
+	header 	= data.first
+	body 		= data.last
 
 	path = requested_file(request_line)
 
@@ -59,11 +62,11 @@ loop do
 			"Content-Type: #{content_type(file)}\r\n" +
 			"Content-Length: #{file.size}\r\n" +
 			"Connection: close\r\n"
-		
 
-		socket.print "\r\n"
 
-		IO.copy_stream(file,socket)
+			socket.print "\r\n"
+
+			IO.copy_stream(file,socket)
 		end
 	else
 		message = "File not found \n"
